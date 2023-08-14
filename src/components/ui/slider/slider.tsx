c1cef8fba978961f501f1daad0d020adbf025fe7
BozhenaVlasova <dix20sept@gmail.com>
2023-08-14 22:36:07 +0300

    Slider created

---
+++ src/components/ui/slider/slider.tsx
@@ -0,0 +1,47 @@
+import { FC, useState } from 'react'
+
+import * as RadixSlider from '@radix-ui/react-slider'
+
+import s from './slider.module.scss'
+
+interface SliderProps {
+  start: number
+  finish: number
+  minValue: number
+  maxValue: number
+}
+
+export const Slider: FC<SliderProps> = ({ start, finish, minValue, maxValue }) => {
+  const [minVal, setMinValue] = useState(minValue)
+  const [maxVal, setMaxValue] = useState(maxValue)
+
+  const changeValue = (value: number[]) => {
+    if (Array.isArray(value)) {
+      setMinValue(value[0])
+      setMaxValue(value[1])
+    } else setMinValue(value)
+  }
+
+  return (
+    <div>
+      <form className={s.form}>
+        <span className={s.sliderValue}>{minVal}</span>
+        <RadixSlider.Root
+          className={s.sliderRoot}
+          defaultValue={[minVal, maxVal]}
+          max={finish}
+          step={1}
+          min={start}
+          onValueChange={changeValue}
+        >
+          <RadixSlider.Track className={s.sliderTrack}>
+            <RadixSlider.Range className={s.sliderRange} />
+          </RadixSlider.Track>
+          <RadixSlider.Thumb className={s.sliderThumb} />
+          <RadixSlider.Thumb className={s.sliderThumb} />
+        </RadixSlider.Root>
+        <span className={s.sliderValue}>{maxVal}</span>
+      </form>
+    </div>
+  )
+}
