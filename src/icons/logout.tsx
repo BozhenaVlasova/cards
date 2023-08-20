60265bdfbc654a2ef1096b3dbfccdf9b4433e474
BozhenaVlasova <dix20sept@gmail.com>
2023-08-21 01:07:17 +0300

    icons added

--- src/icons/logout.tsx
+++ src/icons/logout.tsx
@@ -1,6 +1,6 @@
-import { SVGProps, Ref, forwardRef, memo } from 'react'
-export const SvgLogout = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
-  <svg width={24} height={24} fill="none" xmlns="http://www.w3.org/2000/svg" ref={ref} {...props}>
+import { SVGProps } from 'react'
+export const SvgLogout = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={24} height={24} fill="#fff" xmlns="http://www.w3.org/2000/svg" {...props}>
     <g clipPath="url(#a)" fill="#fff">
       <path d="M7 6a1 1 0 0 0 0-2H5a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h2a1 1 0 0 0 0-2H6V6h1ZM20.82 11.42l-2.82-4a1 1 0 1 0-1.63 1.16L18.09 11H10a1 1 0 0 0 0 2h8l-1.8 2.4a1 1 0 0 0 1.6 1.2l3-4a1 1 0 0 0 .02-1.18Z" />
     </g>
@@ -11,7 +11,5 @@ export const SvgLogout = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement
     </defs>
   </svg>
 )
-const ForwardRef = forwardRef(SvgLogout)
-const Memo = memo(ForwardRef)
 
-export { Memo as ReactComponent }
+export default SvgLogout
