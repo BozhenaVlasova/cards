6be2fe083a2b95a507f91328cf56a823a503bcf3
BozhenaVlasova <dix20sept@gmail.com>
2023-08-24 21:24:07 +0300

    Select component created

---
+++ src/components/ui/select/select.tsx
@@ -0,0 +1,51 @@
+import { FC } from 'react'
+
+import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons'
+import * as Select from '@radix-ui/react-select'
+
+import { Typography } from '../typography/typography'
+
+import s from './select.module.scss'
+
+interface SelectType {
+  id: number
+  value: string
+  item: string
+}
+
+interface SelectItemType {
+  values: SelectType[]
+}
+
+export const SelectDemo: FC<SelectItemType> = ({ values }) => (
+  <Select.Root>
+    <Select.Trigger className={s.SelectTrigger}>
+      <Select.Value placeholder={values[0].item} />
+      <Select.Icon className={s.SelectIcon}>
+        <ChevronDownIcon />
+      </Select.Icon>
+    </Select.Trigger>
+    <Select.Portal>
+      <Select.Content className={s.SelectContent}>
+        <Select.ScrollUpButton className={s.SelectScrollButton}>
+          <ChevronUpIcon />
+        </Select.ScrollUpButton>
+        <Select.Viewport>
+          <Select.Group>
+            {values.map(i => (
+              <Select.Item key={i.id} value={i.value} className={s.Item}>
+                <Select.ItemText>
+                  <Typography variant="body1">{i.item}</Typography>
+                </Select.ItemText>
+                <Select.ItemIndicator className={s.SelectItemIndicator}></Select.ItemIndicator>
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Viewport>
+        <Select.ScrollDownButton className={s.SelectScrollButton}>
+          <ChevronDownIcon />
+        </Select.ScrollDownButton>
+      </Select.Content>
+    </Select.Portal>
+  </Select.Root>
+)
