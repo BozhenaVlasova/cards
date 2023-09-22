d46e8cbdc3f1136f52c856f81b6b7a65c3b81073
BozhenaVlasova <dix20sept@gmail.com>
2023-09-22 19:04:44 +0300

    PagePack created

---
+++ src/pages/pagePack/pagePack.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+
+import * as Collapsible from '@radix-ui/react-collapsible'
+
+import { Button } from '../../components/ui/button'
+import { Card } from '../../components/ui/card/card'
+import { RadioGroupComponent } from '../../components/ui/radioGroup/radioGroup'
+import { Typography } from '../../components/ui/typography/typography'
+import SvgBack from '../../icons/back'
+
+import s from './pagePack.module.scss'
+
+export const CollapsibleComponent = () => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <Collapsible.Root className={s.collapsibleRoot} open={open} onOpenChange={setOpen}>
+      <div className={s.backPage}>
+        <SvgBack />
+        <Typography variant="body2">Back to Packs List</Typography>
+      </div>
+      <Card className={s.common}>
+        <Typography variant="large" className={s.textCard}>
+          Learn &quot;Pack Name&quot;
+        </Typography>
+
+        <div className={s.question}>
+          <Typography variant="subtitle1">
+            Question:{' '}
+            {<Typography variant="body1">How &quot;This&quot; works in JavaScript?</Typography>}
+          </Typography>
+          <Typography variant="body2" className={s.try}>
+            Количество попыток ответов на вопрос: {<Typography variant="subtitle2">10</Typography>}
+          </Typography>
+        </div>
+
+        <Collapsible.Content className={s.content}>
+          <div className={s.answer}>
+            <Typography variant="subtitle1">
+              Answer:{' '}
+              {
+                <Typography variant="body1">
+                  This is how &quot;This&quot; works in JavaScript
+                </Typography>
+              }
+            </Typography>
+          </div>
+          <div className={s.rate}>
+            <Typography variant="subtitle1">Rate yourself:</Typography>
+            <RadioGroupComponent
+              values={[
+                { id: 1, value: 'didNotKnow', label: 'Did not know' },
+                { id: 2, value: 'forgot', label: 'Forgot' },
+                { id: 3, value: 'aLotOfThough', label: 'A lot of though' },
+                { id: 4, value: 'confused', label: 'Confused' },
+                { id: 5, value: 'knewTheAnswer', label: 'Knew the answer' },
+              ]}
+            />
+          </div>
+        </Collapsible.Content>
+
+        <Collapsible.Trigger asChild>
+          <Button className={s.IconButton} variant="primary" fullWidth>
+            <Typography variant="subtitle2">{open ? `Next Question` : `Show Answer`}</Typography>
+          </Button>
+        </Collapsible.Trigger>
+      </Card>
+    </Collapsible.Root>
+  )
+}
