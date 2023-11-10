77b767668e75e53e9a76dfead1d50691ff32c585
BozhenaVlasova <dix20sept@gmail.com>
2023-11-11 00:11:06 +0300

    component typing expanded

--- src/components/ui/radioGroup/radioGroup.tsx
+++ src/components/ui/radioGroup/radioGroup.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { forwardRef } from 'react'
 
 import * as RadioGroup from '@radix-ui/react-radio-group'
 
@@ -6,28 +6,38 @@ import { Typography } from '../typography/typography'
 
 import s from './radioGroup.module.scss'
 
-interface RadioGroupComponentType {
-  values: RadioGroupType[]
+export type RadioGroupProps = Omit<
+  React.ComponentPropsWithoutRef<typeof RadioGroup.Root>,
+  'children'
+> & {
+  options: RadioGroupType[]
+  errorMessage?: string
 }
-interface RadioGroupType {
-  id: number
+type RadioGroupType = {
   value: string
   label: string
 }
 
-export const RadioGroupComponent: FC<RadioGroupComponentType> = ({ values }) => (
-  <form>
-    <RadioGroup.Root className={s.radioGroupRoot} defaultValue={values[0].value}>
-      {values.map(item => (
-        <div className={s.radioGroupCommon} key={item.id}>
-          <RadioGroup.Item className={s.radioGroupItem} value={item.value} id={`r${item.id}`}>
-            <RadioGroup.Indicator className={s.radioGroupIndicator} />
-          </RadioGroup.Item>
-          <Typography variant="body2" className={s.label}>
-            {item.label}
-          </Typography>
-        </div>
-      ))}
-    </RadioGroup.Root>
-  </form>
-)
+export const RadioGroupComponent = forwardRef<
+  React.ElementRef<typeof RadioGroup.Root>,
+  RadioGroupProps
+>((props, ref) => {
+  const { options, errorMessage, ...restProps } = props
+
+  return (
+    <form>
+      <RadioGroup.Root className={s.radioGroupRoot} {...restProps} ref={ref}>
+        {options.map(item => (
+          <div className={s.radioGroupCommon} key={item.value}>
+            <RadioGroup.Item className={s.radioGroupItem} value={item.value} id={`r${item.value}`}>
+              <RadioGroup.Indicator className={s.radioGroupIndicator} />
+            </RadioGroup.Item>
+            <Typography variant="body2" className={s.label}>
+              {item.label}
+            </Typography>
+          </div>
+        ))}
+      </RadioGroup.Root>
+    </form>
+  )
+})
